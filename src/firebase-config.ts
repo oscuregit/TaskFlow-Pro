1e7d091fa64a1cb28c9b34f2164ea2e073281b86
Özgür Avlamış <65289927+oscuregit@users.noreply.github.com>
2026-04-06 16:25:44 +0200

    feat: Initialize Firebase configuration
    Add the Firebase configuration object to the project. This enables the application to connect to Firebase services for authentication, storage, and other backend functionalities.

---
+++ src/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSy" + "CG7lIBGVNyyEBSHfcvXvD2Fl1yu-HDeTI",
+  authDomain: "taskflowpro-d67ee.firebaseapp.com",
+  projectId: "taskflowpro-d67ee",
+  storageBucket: "taskflowpro-d67ee.firebasestorage.app",
+  messagingSenderId: "407727492230",
+  appId: "1:407727492230:web:6b1eb347810cd3576d5507",
+  measurementId: "G-6FPX280MWM"
+};
